e529670efc8126a3751162a89d7a3ac88e9a8bf6
b9030299 <b9030299@newcastle.ac.uk>
2020-02-25 13:03:00 +0000

    items class

--- src/main/java/csc1035/project3/Item.java
+++ src/main/java/csc1035/project3/Item.java
@@ -1,4 +1,11 @@
 package csc1035.project3;
 
 public class Item {
+    int id;
+    String name;
+    String category;
+    boolean perishable;
+    float cost;
+    int stock;
+    float sell_price;
 }
