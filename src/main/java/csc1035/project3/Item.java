6379ff12e9d2f187abe9dc387d63014f7bbde7f3
b9030299 <b9030299@newcastle.ac.uk>
2020-02-26 10:48:46 +0000

    items class

--- src/main/java/csc1035/project3/Item.java
+++ src/main/java/csc1035/project3/Item.java
@@ -1,11 +1,46 @@
 package csc1035.project3;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Items")
 public class Item {
-    int id;
-    String name;
-    String category;
-    boolean perishable;
-    float cost;
-    int stock;
-    float sell_price;
+
+    public Item(int id, String name, String category, boolean perishable, float cost, int stock, float sell_price){
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.perishable = perishable;
+        this.cost = cost;
+        this.stock = stock;
+        this.sell_price = sell_price;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public float getSell_price() {
+        return sell_price;
+    }
+
+    public boolean isPerishable() {
+        return perishable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
