7e2a6ff8a6009c8a4afc6b7eedb259f882b38898
b9030299 <b9030299@newcastle.ac.uk>
2020-02-27 14:04:23 +0000

    Completed insertEntity method

--- src/main/java/csc1035/project3/Input.java
+++ src/main/java/csc1035/project3/Input.java
@@ -7,20 +7,21 @@ import org.hibernate.Session;
 
 public class Input {
     public static void main(String[] args) {
+        Item i1 = new Item("Gold Ring","Jewellery",false,120.00,4,180.00);
+        insertEntity(i1);
+    }
+
+    public static void insertEntity(Item I){
         Session session = HibernateUtil.getSessionFactory().openSession();
         try{
             session = HibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
-            Item i1 = new Item("Gold Ring","Jewellery",false,120.00,4,180.00);
-            session.save(i1);
+            session.save(I);
             session.getTransaction().commit();
         }
         catch(HibernateException e){
             if (session!=null) session.getTransaction().rollback();
             e.printStackTrace();
-        }
-        finally {
-            session.close();
         }
     }
 }
