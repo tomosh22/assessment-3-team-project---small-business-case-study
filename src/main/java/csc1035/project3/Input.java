16423b0d0aedd34337b01aceea3f7280d4a36711
b9030299 <b9030299@newcastle.ac.uk>
2020-02-27 14:42:29 +0000

    Implemented updateEntity and removeEntity

--- src/main/java/csc1035/project3/Input.java
+++ src/main/java/csc1035/project3/Input.java
@@ -8,7 +8,9 @@ import org.hibernate.Session;
 public class Input {
     public static void main(String[] args) {
         Item i1 = new Item("Gold Ring","Jewellery",false,120.00,4,180.00);
-        insertEntity(i1);
+        //insertEntity(i1);
+        //removeEntity(4);
+        updateEntity(0, 2, "Test", 0.0, false, 0);
     }
 
     public static void insertEntity(Item I){
@@ -22,6 +24,93 @@ public class Input {
         catch(HibernateException e){
             if (session!=null) session.getTransaction().rollback();
             e.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
+    }
+
+    public static void removeEntity(int key){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try{
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Item I = session.get(Item.class, key);
+            session.delete(I);
+            session.getTransaction().commit();
+        }
+        catch(HibernateException e){
+            if (session!=null) session.getTransaction().rollback();
+            e.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
+    }
+
+    public static void updateEntity(int column, int key, String text, double cost, boolean perishable, int num){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Item I = session.get(Item.class, key);
+        try {
+            switch (column) {
+                //update category
+                case (0):
+                    session = HibernateUtil.getSessionFactory().openSession();
+                    session.beginTransaction();
+                    I.setCategory(text);
+                    session.update(I);
+                    session.getTransaction().commit();
+                    break;
+                //update cost
+                case (1):
+                    session = HibernateUtil.getSessionFactory().openSession();
+                    session.beginTransaction();
+                    I.setCost(cost);
+                    session.update(I);
+                    session.getTransaction().commit();
+                    break;
+                //update name
+                case (2):
+                    session = HibernateUtil.getSessionFactory().openSession();
+                    session.beginTransaction();
+                    I.setName(text);
+                    session.update(I);
+                    session.getTransaction().commit();
+                    break;
+                //update perishable
+                case (3):
+                    session = HibernateUtil.getSessionFactory().openSession();
+                    session.beginTransaction();
+                    I.setPerishable(perishable);
+                    session.update(I);
+                    session.getTransaction().commit();
+                    break;
+                //update sell_price
+                case (4):
+                    session = HibernateUtil.getSessionFactory().openSession();
+                    session.beginTransaction();
+                    I.setSell_price(cost);
+                    session.update(I);
+                    session.getTransaction().commit();
+                    break;
+                //update stock
+                case (5):
+                    session = HibernateUtil.getSessionFactory().openSession();
+                    session.beginTransaction();
+                    I.setStock(num);
+                    session.update(I);
+                    session.getTransaction().commit();
+                    break;
+                default:
+                    break;
+            }
+        }
+        catch(HibernateException e){
+            if (session!=null) session.getTransaction().rollback();
+            e.printStackTrace();
+        }
+        finally {
+            session.close();
         }
     }
 }
