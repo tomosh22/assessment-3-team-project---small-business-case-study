285827d6ab186545e3364221cfc8e7d7d6edc49e
b9030299 <b9030299@newcastle.ac.uk>
2020-03-11 10:41:56 +0000

    Implemented first test for insert entity

--- src/main/Testing/InputTesting.java
+++ src/main/Testing/InputTesting.java
@@ -1,2 +1,14 @@
+import csc1035.project3.*;
+import csc1035.project3.Item;
+import org.junit.Test;
+
 public class InputTesting {
+
+    @Test
+    public void testInsertEntity(){
+        Item i = new Item("TestName", "TestCategory", false, 20.00, 15, 20.00);
+        Input.insertEntity(i);
+        assert Output.getDBSellPrice("TestName")==i.getSell_price();
+        Input.removeEntity(Output.getDBId("TestName"));
+    }
 }
